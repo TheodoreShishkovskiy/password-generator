1a28873a1cee2e3cb186e9377f7e5ad620385068
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-08-18 11:31:09 -0400

    finished typing else if for generator, but still showing errors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,19 +3,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Var Arrays
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"];
-var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "/", ",", ".", "-", "=", "_", "+"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 // Var Declarations
-var passwordLength = "";
+var passwordLength;
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 var userChoices;
+
+// Var Arrays
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"];
+var specialCharacter = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "/", ",", ".", "-", "=", "_", "+"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -50,6 +50,71 @@ function generatePassword() {
       confirmSpecialCharacter = confirm("Will this password contain special charachters?");
       console.log("Special Character " confirmSpecialCharacter);
     }
+};
+
+if (!confirmLowerCase && !confirmUpperCase && !confirmNumericCharacter && !confirmNumericCharacter) {
+  userChoices = alert("You must choose a criteria:");
+
+} else if (confirmLowerCase && confirmUpperCase && confirmNumericCharacter && confirmSpecialCharacter) {
+  userChoices = lowerCase.concast(upperCase, numbers, special);
+  console.log(userChoices);
+}
+
+else if (confirmLowerCase && confirmUpperCase && confirmNumericCharacter) {
+  userChoices = lowerCase.concat(upperCase, numbers, special);
+  console.log(userChoices);
+}
+else if (confirmLowerCase && confirmUpperCase && confirmSpecialCharacter) {
+  userChoices = lowerCase.concat(upperCase, special);
+  console.log(userChoices);
+}
+else if (confirmLowerCase && confirmNumericCharacter && confirmNumericCharacter) {
+  userChoices = lowerCase.concat(numbers, special);
+  console.log(userChoices);
+}
+else if (confirmUpperCase && confirmNumericCharacter && confirmSpecialCharacter) {
+  userChoices = upperCase.concat(numbers,special);
+  console.log(userChoices);
+}
+else if (confirmLowerCase && confirmUpperCase) {
+  userChoices = lowerCase.concat(upperCase);
+  console.log(userChoices);
+}
+else if (confirmLowerCase && confirmNumericCharacter) {
+  userChoices = lowerCase.concat(numbers);
+  console.log(userChoices);
+}
+else if (confirmLowerCase && confirmSpecialCharacter) {
+  userChoices = lowerCase.concat(special);
+  console.log(userChoices);
+}
+else if (confirmUpperCase && confirmNumericCharacter) {
+  userChoices = upperCase.concat(numbers);
+  console.log(userChoices);
+}
+else if (confirmUpperCase && confirmSpecialCharacter) {
+  userChoices = upperCase.concat(special);
+  console.log(userChoices);
+}
+else if (confirmNumericCharacter && confirmSpecialCharacter) {
+  userChoices = numbers.concat(special);
+  console.log(userChoices);
+}
+else if (confirmLowerCase) {
+  userChoices = lowerCase;
+  console.log(userChoices);
+}
+else if (confirmUpperCase) {
+  userChoices = upperCase;
+  console.log(userChoices);
+}
+else if (confirmNumericCharacter) {
+  userChoices = numbers;
+  console.log(userChoices);
+}
+else if (confirmSpecialCharacter) {
+  userChoices = special;
+  console.log(userChoices);
 };
 
 var passwordBlank = [];
