66a75498c48c83e5d1c8b7381ffb4202fe904093
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-08-17 21:11:10 -0400

    added the first part of the password functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,17 +4,18 @@
 var generateBtn = document.querySelector("#generate");
 
 // Var Arrays
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "/", ",", ".", "-", "=", "_", "+"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9"];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "/", ",", ".", "-", "=", "_", "+"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Var Declarations
-var confirmLength = "";
+var passwordLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
+var userChoices;
 
 // Write password to the #password input
 function writePassword() {
@@ -27,3 +28,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  passwordLength = prompt("How many characters would you like you password to be? Choose between 8 to 128");
+  console.log("Password length " + passwordLength);
+
+  if(!passwordLength) {
+    alert("Required Value");
+
+  } else if (passwordLength <= 8 || passwordLength >= 128) {
+    passwordLength = prompt("You must choose a password between 8 to 128 characters");
+    console.log("Password length " + passwordLength);
+
+    else {
+      confirmLowerCase = confirm("will this password contain lower case letters?");
+      console.log("Lower case " + confirmLowerCase);
+    }
+  }
+
+}
